782d902448585e3dc0c35f6b872f8bd1d0cc9921
maruthikumar <maruthikumar121@gmail.com>
2020-10-06 09:48:14 +0530

    my files

--- push.sh
+++ push.sh
@@ -1,9 +1,6 @@
-echo "Enter github path"
- read path
-
  git init
  git add .
  git commit -m "my files"
- git remote add origin $path
+ git remote add origin https://github.com/maruthikumar121/shell.git
  git pull origin master
  git push origin master 
